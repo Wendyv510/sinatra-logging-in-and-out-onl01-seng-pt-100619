957c6c289f6fe6a6eb646b2d98c26ee095843756
Wendy Harris <wendyv510@hotmail.com>
2020-03-01 22:16:10 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,7 +22,7 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/account' do
-      if Helpers.is_logged_in?(session[:user_id])
+      if is_logged_in?(session[:user_id])
         erb :account
       else 
         erb :error
